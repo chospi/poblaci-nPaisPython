b2c02521d26481ceffcae80803cbd04374d21154
chospi <sebas_mary@hotmail.com>
2024-04-08 14:06:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -40,12 +40,3 @@ def read_csv(path):
       data.append(country_dic)#Agregamos el diccionario a la lista
     return data  
 
-def run(path):
-  data = read_csv("./app/data.csv")
-  country = input("¿Que pais quiere ver la población?")
-  labels, values = population_by_country(data,country)
-  generate(labels,values)
-
-if __name__ == "__main__":
-  run("./app/data.csv")
-  
